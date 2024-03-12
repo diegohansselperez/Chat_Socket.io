adeb41e1888bdc17b4a54fd9e5f80e255afffff2
unknown <diegohanssselp24@gmail.com>
2024-03-11 23:49:14 -0700

    Instalamos y creamos una base de dtos con Turso

--- server/index.js
+++ server/index.js
@@ -3,13 +3,26 @@ import morgan from 'morgan';
 import { Server } from 'socket.io';
 import { createServer } from 'node:http';
 import { Socket } from 'node:dgram';
+import dotenv from 'dotenv';
+import { createClient } from '@libsql/client/.';
 
 const PORT = process.env.PORT ?? 3001;
 const app = express();
 
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
 
+const db = createClient({
+  url: 'libsql://obliging-stilt-man-diegohansselperez.turso.io',
+  authToken: process.env.DB_TOKEN,
+});
+
+await db.execute('CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  content TEXT
+)');
 io.on('connection', (socket) => {
   console.log('a user has connected!');
   socket.on('disconnect', () => {
